084ce9d8e9ca150d91216e78404a141aa50819be
Barbara <b_zimmermann@gmx.at>
2013-04-02 22:22:13 +0200

    Connect Amazon S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,7 @@ Omrails::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # ImageMagick is required for Paperclip to work
+  Paperclip.options[:command_path] = "/c/Program Files (x86)/ImageMagick-6.8.4-Q16/convert"
 end
