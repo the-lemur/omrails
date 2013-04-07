9b82c5fc954c1674adfc4a275c0353f9e8467589
Barbara <b_zimmermann@gmx.at>
2013-04-07 15:48:06 +0200

    Added uniqueness validation for name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	@pins = @user.pins
+  	@pins = @user.pins.order("created_at desc")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  validates_uniqueness_of :name
   # attr_accessible :title, :body
 
   has_many :pins
