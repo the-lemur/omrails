a95000b8e8e1dd28687e1c76df4796d89b64681b
Barbara <b_zimmermann@gmx.at>
2013-04-08 23:36:53 +0200

    Add Profile Pic and change it

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,13 @@ class User < ActiveRecord::Base
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :image
   validates_uniqueness_of :name
+  validates_attachment :image,
+                       content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                       size: { less_than: 5.megabytes }
   # attr_accessible :title, :body
 
   has_many :pins
+  has_attached_file :image, styles: { small: "150x150>"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130406202537) do
+ActiveRecord::Schema.define(:version => 20130408155929) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
@@ -43,6 +43,10 @@ ActiveRecord::Schema.define(:version => 20130406202537) do
     t.datetime "updated_at",                             :null => false
     t.string   "name"
     t.string   "City"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
